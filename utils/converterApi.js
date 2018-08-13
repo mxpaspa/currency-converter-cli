a6ac194ba0863876398506a2897f515a31a30fa1
Max Paspa <mxpaspa@gmail.com>
2018-08-13 13:47:54 -0400

    Update converterApi.js

--- utils/converterApi.js
+++ utils/converterApi.js
@@ -4,22 +4,6 @@ const Log = require('../models/conversionHistoryModel')
 
 module.exports = async (from,to,amount) => {
 
-  // define the mlab database url
-  var mongoose = require('mongoose');
-  var uri = '[mlabUrl]/converter-cli';
-
-  // define a timeout (required for mlab)
-  var options = {
-        "keepAlive" : 300000,
-        "connectTimeoutMS" : 30000,
-        useNewUrlParser: true
-  }
-
-  // connect to mongodb
-  mongoose.connect(uri, options);
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-
   const results = await axios({
     method: 'get',
     url: 'https://data.fixer.io/api/convert?access_key=[]&from='+from+'&to='+to+'&amount='+amount,
