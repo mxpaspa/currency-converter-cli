dbf3381fac979782667d8920fbbd71fd3d3173da
Max Paspa <mxpaspa@gmail.com>
2018-08-13 13:40:36 -0400

    Update converterApi.js

--- utils/converterApi.js
+++ utils/converterApi.js
@@ -22,7 +22,7 @@ module.exports = async (from,to,amount) => {
 
   const results = await axios({
     method: 'get',
-    url: 'https://data.fixer.io/api/convert?access_key=256eadee430def262f66216fda975c88&from='+from+'&to='+to+'&amount='+amount,
+    url: 'https://data.fixer.io/api/convert?access_key=[]&from='+from+'&to='+to+'&amount='+amount,
     params: {
       format: 'json',
     },
